b68f4d4c6fab2bcc819ba3f034aceb3e905e2c9f
Jorge Silva <jsilva179@users.noreply.github.com>
2014-12-27 13:16:56 -0600

    FAMILY Controller
    ----Jorge Silva

--- app/Controller/FamiliesController.php
+++ app/Controller/FamiliesController.php
@@ -9,8 +9,7 @@ App::uses('AppController', 'Controller');
  * @property PaginatorComponent $Paginator
  * @property SessionComponent $Session
  */
-class FamiliesController extends AppController
-{
+class FamiliesController extends AppController {
 
     /**
      * Components
@@ -24,8 +23,7 @@ class FamiliesController extends AppController
      *
      * @return void
      */
-    public function index()
-    {
+    public function index() {
         $this->Family->recursive = 0;
         $this->set('families', $this->Paginator->paginate());
     }
@@ -37,13 +35,12 @@ class FamiliesController extends AppController
      * @param string $id
      * @return void
      */
-    public function view($id = null)
-    {
-        if (!$this->Family->exists($id))
-        {
+    public function view($id = null) {
+        if (!$this->Family->exists($id)) {
             throw new NotFoundException(__('Invalid family'));
         }
         $options = array('conditions' => array('Family.' . $this->Family->primaryKey => $id));
+        $this->request->data = $this->Family->find('first', $options);
         $this->set('family', $this->Family->find('first', $options));
     }
 
@@ -52,20 +49,28 @@ class FamiliesController extends AppController
      *
      * @return void
      */
-    public function add()
-    {
-        if ($this->request->is('post'))
-        {
+    public function add() {
+        if ($this->request->is('post')) {
             $this->Family->create();
-            if ($this->Family->save($this->request->data))
-            {
+            if ($this->Family->save($this->request->data)) {
                 $this->Session->setFlash(__('The family has been saved.'));
                 return $this->redirect(array('action' => 'index'));
-            } else
-            {
+            } else {
                 $this->Session->setFlash(__('The family could not be saved. Please, try again.'));
             }
         }
+        $this->loadModel('Lov');
+        $this->Lov->recursive = -1;
+
+        $lovFamilyStatus = $this->Lov->find('list', array(
+            'fields' => array('Lov.value', 'Lov.name_' . $this->appLangConf),
+            'conditions' => array(
+                'Lov.type =' => 'FAMILY_FIELD_STATUS',
+                'Lov.status' => array(StatusOfLov::Active)
+            ),
+            'order' => array('ordershow')
+        ));
+        $this->set(compact('lovFamilyStatus'));
     }
 
     /**
@@ -75,27 +80,33 @@ class FamiliesController extends AppController
      * @param string $id
      * @return void
      */
-    public function edit($id = null)
-    {
-        if (!$this->Family->exists($id))
-        {
+    public function edit($id = null) {
+        if (!$this->Family->exists($id)) {
             throw new NotFoundException(__('Invalid family'));
         }
-        if ($this->request->is(array('post', 'put')))
-        {
-            if ($this->Family->save($this->request->data))
-            {
+        if ($this->request->is(array('post', 'put'))) {
+            if ($this->Family->save($this->request->data)) {
                 $this->Session->setFlash(__('The family has been saved.'));
                 return $this->redirect(array('action' => 'index'));
-            } else
-            {
+            } else {
                 $this->Session->setFlash(__('The family could not be saved. Please, try again.'));
             }
-        } else
-        {
+        } else {
             $options = array('conditions' => array('Family.' . $this->Family->primaryKey => $id));
             $this->request->data = $this->Family->find('first', $options);
         }
+        $this->loadModel('Lov');
+        $this->Lov->recursive = -1;
+
+        $lovFamilyStatus = $this->Lov->find('list', array(
+            'fields' => array('Lov.value', 'Lov.name_' . $this->appLangConf),
+            'conditions' => array(
+                'Lov.type =' => 'FAMILY_FIELD_STATUS',
+                'Lov.status' => array(StatusOfLov::Active)
+            ),
+            'order' => array('ordershow')
+        ));
+        $this->set(compact('lovFamilyStatus'));
     }
 
     /**
@@ -105,19 +116,15 @@ class FamiliesController extends AppController
      * @param string $id
      * @return void
      */
-    public function delete($id = null)
-    {
+    public function delete($id = null) {
         $this->Family->id = $id;
-        if (!$this->Family->exists())
-        {
+        if (!$this->Family->exists()) {
             throw new NotFoundException(__('Invalid family'));
         }
         $this->request->allowMethod('post', 'delete');
-        if ($this->Family->delete())
-        {
+        if ($this->Family->delete()) {
             $this->Session->setFlash(__('The family has been deleted.'));
-        } else
-        {
+        } else {
             $this->Session->setFlash(__('The family could not be deleted. Please, try again.'));
         }
         return $this->redirect(array('action' => 'index'));
@@ -128,8 +135,7 @@ class FamiliesController extends AppController
      *
      * @return void
      */
-    public function admin_index()
-    {
+    public function admin_index() {
         $this->Family->recursive = 0;
         $this->set('families', $this->Paginator->paginate());
     }
@@ -141,13 +147,12 @@ class FamiliesController extends AppController
      * @param string $id
      * @return void
      */
-    public function admin_view($id = null)
-    {
-        if (!$this->Family->exists($id))
-        {
+    public function admin_view($id = null) {
+        if (!$this->Family->exists($id)) {
             throw new NotFoundException(__('Invalid family'));
         }
         $options = array('conditions' => array('Family.' . $this->Family->primaryKey => $id));
+        $this->request->data = $this->Family->find('first', $options);
         $this->set('family', $this->Family->find('first', $options));
     }
 
@@ -156,20 +161,28 @@ class FamiliesController extends AppController
      *
      * @return void
      */
-    public function admin_add()
-    {
-        if ($this->request->is('post'))
-        {
+    public function admin_add() {
+        if ($this->request->is('post')) {
             $this->Family->create();
-            if ($this->Family->save($this->request->data))
-            {
+            if ($this->Family->save($this->request->data)) {
                 $this->Session->setFlash(__('The family has been saved.'));
                 return $this->redirect(array('action' => 'index'));
-            } else
-            {
+            } else {
                 $this->Session->setFlash(__('The family could not be saved. Please, try again.'));
             }
         }
+        $this->loadModel('Lov');
+        $this->Lov->recursive = -1;
+
+        $lovFamilyStatus = $this->Lov->find('list', array(
+            'fields' => array('Lov.value', 'Lov.name_' . $this->appLangConf),
+            'conditions' => array(
+                'Lov.type =' => 'FAMILY_FIELD_STATUS',
+                'Lov.status' => array(StatusOfLov::Active)
+            ),
+            'order' => array('ordershow')
+        ));
+        $this->set(compact('lovFamilyStatus'));
     }
 
     /**
@@ -179,27 +192,33 @@ class FamiliesController extends AppController
      * @param string $id
      * @return void
      */
-    public function admin_edit($id = null)
-    {
-        if (!$this->Family->exists($id))
-        {
+    public function admin_edit($id = null) {
+        if (!$this->Family->exists($id)) {
             throw new NotFoundException(__('Invalid family'));
         }
-        if ($this->request->is(array('post', 'put')))
-        {
-            if ($this->Family->save($this->request->data))
-            {
+        if ($this->request->is(array('post', 'put'))) {
+            if ($this->Family->save($this->request->data)) {
                 $this->Session->setFlash(__('The family has been saved.'));
                 return $this->redirect(array('action' => 'index'));
-            } else
-            {
+            } else {
                 $this->Session->setFlash(__('The family could not be saved. Please, try again.'));
             }
-        } else
-        {
+        } else {
             $options = array('conditions' => array('Family.' . $this->Family->primaryKey => $id));
             $this->request->data = $this->Family->find('first', $options);
         }
+        $this->loadModel('Lov');
+        $this->Lov->recursive = -1;
+
+        $lovFamilyStatus = $this->Lov->find('list', array(
+            'fields' => array('Lov.value', 'Lov.name_' . $this->appLangConf),
+            'conditions' => array(
+                'Lov.type =' => 'FAMILY_FIELD_STATUS',
+                'Lov.status' => array(StatusOfLov::Active)
+            ),
+            'order' => array('ordershow')
+        ));
+        $this->set(compact('lovFamilyStatus'));
     }
 
     /**
@@ -209,19 +228,15 @@ class FamiliesController extends AppController
      * @param string $id
      * @return void
      */
-    public function admin_delete($id = null)
-    {
+    public function admin_delete($id = null) {
         $this->Family->id = $id;
-        if (!$this->Family->exists())
-        {
+        if (!$this->Family->exists()) {
             throw new NotFoundException(__('Invalid family'));
         }
         $this->request->allowMethod('post', 'delete');
-        if ($this->Family->delete())
-        {
+        if ($this->Family->delete()) {
             $this->Session->setFlash(__('The family has been deleted.'));
-        } else
-        {
+        } else {
             $this->Session->setFlash(__('The family could not be deleted. Please, try again.'));
         }
         return $this->redirect(array('action' => 'index'));
@@ -230,8 +245,7 @@ class FamiliesController extends AppController
     /**
      * 
      */
-    public function jsfindFamily()
-    {
+    public function jsfindFamily() {
         Configure::write('debug', 0);
         $this->autoRender = false;
         $this->layout = 'ajax';
@@ -239,22 +253,17 @@ class FamiliesController extends AppController
         $arrayConditions = array();
         $response = array();
         $results = array();
-        try
-        {
+        try {
 
-            switch ($this->request->query['format'])
-            {
-                case 'fullsearch':
-                    {
-                        if ("0" !== $this->request->query['familyid'] && "" !== $this->request->query['familyid'])
-                        {
+            switch ($this->request->query['format']) {
+                case 'fullsearch': {
+                        if ("0" !== $this->request->query['familyid'] && "" !== $this->request->query['familyid']) {
 
                             $arrayConditions = array(
                                 'Family.id >= ' => 1,
                                 'Family.id = ' => $this->request->query['familyid']
                             );
-                        } else
-                        {
+                        } else {
                             $arrayConditions = array(
                                 "OR" => array(
                                     'Family.title LIKE' => '%' . $this->request->query['strquery'] . '%'
@@ -268,8 +277,7 @@ class FamiliesController extends AppController
                         $response = $results;
                     }
                     break;
-                case 'autocompletelist':
-                    {
+                case 'autocompletelist': {
                         $this->Family->recursive = 0; /// Solo usuario
                         $arrayConditions = array(
                             "OR" => array(
@@ -279,15 +287,13 @@ class FamiliesController extends AppController
                         $results = $this->Family->find('all', array(
                             'conditions' => $arrayConditions
                         ));
-                        foreach ($results as $key => $value)
-                        {
+                        foreach ($results as $key => $value) {
                             $response[$key]["id"] = $value["Family"]["id"];
                             $response[$key]["value"] = $value["Family"]["firstname"] . ' ' . $value["Family"]["lastname"] . " (" . $value['Family']['familyname'] . ")";
                         }
                     }
                     break;
-                case 'typeahead':
-                    {
+                case 'typeahead': {
                         $this->Family->recursive = 0; /// Solo usuario
                         $arrayConditions = array(
                             'Family.id >= ' => 1
@@ -295,15 +301,13 @@ class FamiliesController extends AppController
                         $results = $this->Family->find('all', array(
                             'conditions' => $arrayConditions
                         ));
-                        foreach ($results as $key => $value)
-                        {
+                        foreach ($results as $key => $value) {
                             $response[$key]["id"] = $value["Family"]["id"];
                             $response[$key]["value"] = $value["Family"]["firstname"] . ' ' . $value["Family"]["lastname"] . " (" . $value['Family']['Familyname'] . ")";
                         }
                     }
                     break;
-                case 'all':
-                    {
+                case 'all': {
                         $this->Family->recursive = 1;
                         $arrayConditions = array(
                             'Family.id >= ' => 1,
@@ -322,8 +326,7 @@ class FamiliesController extends AppController
                 default :
                     break;
             }
-        } catch (Exception $ex)
-        {
+        } catch (Exception $ex) {
             $response = array(
                 'success' => false,
                 'message' => $ex->getMessage(),
